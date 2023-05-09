ce7dafe6e467fe1f5026d57c64a82212d52dab7e
Pranav Waghmare <piyushw55@gmail.com>
2023-05-09 19:07:00 +0530

    [Pranav]Added: Delete Array method Generic

---
+++ GenericPracticeProblem/DeleteArrayGeneric.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPracticeProblem
+{
+    public class DeleteArrayGeneric<T>
+    {
+        
+        public void GenDeleteMethod(ref T[] arr, T iPos)
+        {
+            int iCnt = 0;
+            T[] newArr = new T[arr.Length - 1];
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                if (arr[i].Equals(iPos))
+                {
+                    break;
+                }
+                newArr[i] = arr[i];
+                iCnt++;
+            }
+            //Console.WriteLine(iCnt);
+            for (int i = iCnt; i < newArr.Length; i++)
+            {
+
+                newArr[i] = arr[i + 1];
+            }
+
+            arr = newArr;
+
+
+        }
+    }
+}
