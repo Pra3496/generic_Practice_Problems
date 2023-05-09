f2e1f9fabe9b60b58594588474f44f158cd69b4c
Pranav Waghmare <piyushw55@gmail.com>
2023-05-09 19:02:27 +0530

    [Pranav]Added: Delete Array method Normal

---
+++ GenericPracticeProblem/DeleteArrays.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace GenericPracticeProblem
+{
+    public class DeleteArray
+    {
+       
+        public void iDeleteMethod(ref int[] arr, int iPos)
+        {
+            int iCnt = 0;
+            int[] newArr = new int[arr.Length - 1];
+            for(int i = 0;i < arr.Length; i++)
+            {
+                
+                if (arr[i] == iPos)
+                {
+                    break;
+                }
+                newArr[i] = arr[i];
+                iCnt++;
+            }
+            //Console.WriteLine(iCnt);
+            for(int i = iCnt; i < newArr.Length; i++)
+            {
+
+                newArr[i] = arr[i+1]; 
+            }
+
+            arr = newArr;
+
+
+        }
+
+        public void dDeleteMethod(ref double[] arr, double iPos)
+        {
+            int iCnt = 0;
+            double[] newArr = new double[arr.Length - 1];
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                if (arr[i] == iPos)
+                {
+                    break;
+                }
+                newArr[i] = arr[i];
+                iCnt++;
+            }
+            //Console.WriteLine(iCnt);
+            for (int i = iCnt; i < newArr.Length; i++)
+            {
+
+                newArr[i] = arr[i + 1];
+            }
+
+            arr = newArr;
+
+
+        }
+
+        public void cDeleteMethod(ref char[] arr, char iPos)
+        {
+            int iCnt = 0;
+            char[] newArr = new char[arr.Length - 1];
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                if (arr[i] == iPos)
+                {
+                    break;
+                }
+                newArr[i] = arr[i];
+                iCnt++;
+            }
+            //Console.WriteLine(iCnt);
+            for (int i = iCnt; i < newArr.Length; i++)
+            {
+
+                newArr[i] = arr[i + 1];
+            }
+
+            arr = newArr;
+
+
+        }
+    }
+}
---
+++ GenericPracticeProblem/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            DeleteArray deleteArray = new DeleteArray();
+
+            // Integer Array
+            int[] iArr = { 1, 2, 3, 4, 5, 6, 5};
+
+            Console.WriteLine("Array before Deleting Element in Integer");
+            foreach (int i in iArr)
+            {
+                Console.Write("\t" + i);
+            }
+            Console.WriteLine();
+            deleteArray.iDeleteMethod(ref iArr,4);
+
+            Console.WriteLine("Array after Deleting Element in Integer");
+            foreach(int i in iArr)
+            {
+                Console.Write("\t"+i);
+            }
+            Console.WriteLine("\n\n");
+
+            // Double Array
+            double[] dArr = { 1.56, 65.2, 15.3, 44.66, 3.5, 54.6, 21.5 };
+
+
+
+            Console.WriteLine("Array before Deleting Element in Double");
+            foreach (double i in dArr)
+            {
+                Console.Write("\t" + i);
+            }
+            Console.WriteLine();
+
+
+            deleteArray.dDeleteMethod(ref dArr, 21.5);
+
+            Console.WriteLine("Array after Deleting Element in Double");
+            foreach (double i in dArr)
+            {
+                Console.Write("\t" + i);
+            }
+            Console.WriteLine("\n\n");
+
+
+            // Charecter Array
+            char[] cArr = { 'a','P','u','T','L'};
+
+            Console.WriteLine("Array before Deleting Element in Charecter");
+            foreach (char i in cArr)
+            {
+                Console.Write("\t" + i);
+            }
+            Console.WriteLine();
+
+
+            deleteArray.cDeleteMethod(ref cArr, 'a');
+
+            Console.WriteLine("Array after Deleting Element in Charecter");
+            foreach (char i in cArr)
+            {
+                Console.Write("\t" + i);
+            }
+            Console.WriteLine("\n\n");
+
+            */
+
+            
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
