f30fa28d267f02b0bf00baeac34363f887502759
Pranav Waghmare <piyushw55@gmail.com>
2023-05-10 20:04:01 +0530

    [Pranav]Added: Ability to find Minimum for given items String

---
+++ GenericPracticeProblem/GenericMinimum.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericPracticeProblem
+{
+    public class GenericMinimum<T> where T : IComparable
+    {
+        
+       
+        public static void GenericMinimumIs(T firstValue, T secondValue, T thirdValue)
+        {
+            if ((firstValue.Equals(secondValue)) && (firstValue.Equals(thirdValue)))
+            {
+                Console.WriteLine("All are same");
+            }
+            else
+            {
+                if ((firstValue.CompareTo(secondValue)) == (firstValue.CompareTo(thirdValue)) && (firstValue.CompareTo(thirdValue) == 1))
+                {
+                    if(secondValue.CompareTo(thirdValue) == 1)
+                    {
+                        Console.WriteLine("Minimum Number is : " +thirdValue);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Minimum Number is : " + secondValue);
+                    }
+                }
+                else if ((secondValue.CompareTo(firstValue)) == (secondValue.CompareTo(thirdValue)) && (secondValue.CompareTo(thirdValue) == 1))
+                {
+                    if (firstValue.CompareTo(thirdValue) == 1)
+                    {
+                        Console.WriteLine("Minimum Number is : " +thirdValue);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Minimum Number is : " +firstValue);
+                    }
+                }
+                else
+                {
+                    if (firstValue.CompareTo(secondValue) == 1)
+                    {
+                        Console.WriteLine("Minimum Number is : " +secondValue);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Minimum Number is : " +firstValue);
+                    }
+                }
+            }
+
+        }
+    }
+}
--- GenericPracticeProblem/Program.cs
+++ GenericPracticeProblem/Program.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Metadata.W3cXsd2001;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
+
 
 namespace GenericPracticeProblem
 {
     internal class Program
     {
         static void Main(string[] args)
-        { 
-            
+        {
+
             // Minimum Number From three Integers
-
-            MinimumNumber min = new MinimumNumber();
-
             Console.WriteLine("Enter Three Number");
             Console.Write("Enter First Numbers : ");
             int iNo1 = Convert.ToInt32(Console.ReadLine());
@@ -24,35 +18,12 @@ namespace GenericPracticeProblem
             Console.Write("Enter Third Number : ");
             int iNo3 = Convert.ToInt32(Console.ReadLine());
 
-            min.MinimumIsInteger(iNo1,iNo2,iNo3);
+
+            //GenericMinimum<int> Gmin = new GenericMinimum<int>(iNo1, iNo2, iNo3);
 
 
 
-            Console.WriteLine("Enter Three Number");
-
-            Console.Write("Enter First Numbers : ");
-            float fNo1 = Convert.ToSingle(Console.ReadLine());
-
-            Console.Write("Enter Second Number : ");
-            float fNo2 = Single.Parse(Console.ReadLine());
-
-            Console.Write("Enter Third Number : ");
-            float fNo3 = float.Parse(Console.ReadLine());
-
-            min.MinimumIsFloat(fNo1, fNo2, fNo3);
-
-
-            Console.WriteLine("Enter Three Line : ");
-
-            Console.Write("Enter First line : ");
-            string sNo1 = Console.ReadLine();
-            Console.Write("Enter Second line : ");
-            string sNo2 = Console.ReadLine();
-            Console.Write("Enter Second line : ");
-            string sNo3 = Console.ReadLine();
-
-            min.MinimumIsString(sNo1, sNo2, sNo3);
-
+            GenericMinimum<int>.GenericMinimumIs(iNo1, iNo2, iNo3);
 
 
             Console.ReadKey();
